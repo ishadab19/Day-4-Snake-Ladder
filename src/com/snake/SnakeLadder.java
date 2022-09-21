da054d68da6f9febbd2919ec833fe1f51870081e
shadab <me.shadab19@gmail.com>
2022-09-21 21:18:17 +0530

    [Added- Player Rolls the Dice For the First Time]

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -1,11 +1,14 @@
 package com.snake;
-
+import java.util.*;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		int start;
 		start =  0; //player starting point
 		System.out.println("Player Starting Position will be:~ ");
 		System.out.println(+start);
+		Random random = new Random();
+		int dice = random.nextInt(6)+1;
+		System.out.println("dice roll result: "+dice);
 		
 	}
 
