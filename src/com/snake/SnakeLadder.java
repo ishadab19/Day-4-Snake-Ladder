1d72f03437da443f58f59c90640d57b0dcc3f6a0
shadab <me.shadab19@gmail.com>
2022-09-21 21:21:32 +0530

    [Added- Checking for Option Snake Ladder No play]

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -1,15 +1,35 @@
 package com.snake;
+
 import java.util.*;
+
 public class SnakeLadder {
 	public static void main(String[] args) {
 		int start;
-		start =  0; //player starting point
+		start = 0; // player starting point
 		System.out.println("Player Starting Position will be:~ ");
 		System.out.println(+start);
 		Random random = new Random();
-		int dice = random.nextInt(6)+1;
-		System.out.println("dice roll result: "+dice);
-		
-	}
+		int dice = random.nextInt(6) + 1;
+		System.out.println("dice roll result: " + dice);
+		int userPosition = 0;
+		int cases = random.nextInt(3);
+		switch (cases) {
+		case 0:
+			System.out.println("No Play!!");
+			break;
+		case 1:
+			System.out.println("Ladder!");
+			userPosition = userPosition + dice;
 
+			break;
+		default:
+			System.out.println("Snake!");
+			userPosition = userPosition - dice;
+
+			if (userPosition < 0) {
+				userPosition = 0;
+			}
+		}
+		System.out.println("Presnt Position of the User: " + userPosition);
+	}
 }
