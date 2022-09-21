2d88e130cc7a06db04bb280d9ee994efdfa43f2e
shadab <me.shadab19@gmail.com>
2022-09-21 21:12:33 +0530

    [Added- starting Position]

---
+++ src/com/snake/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.snake;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int start;
+		start =  0; //player starting point
+		System.out.println("Player Starting Position will be:~ ");
+		System.out.println(+start);
+		
+		
+	}
+
+}
